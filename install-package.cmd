8ebafaaa2ec3f4082753ad7380103cb04a425669
dirkarnez <smalldirkalex@gmail.com>
2023-03-17 20:37:37 +0800

    - init

--- install-package.cmd
+++ install-package.cmd
@@ -1,7 +1,11 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.7.9-amd64\
+@REM set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable\
+@REM set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
+
+set PYTHON_DIR=D:\Softwares\python-3.10.8-amd64-portable\
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
 
 python -m pip install --upgrade pip
+python -m pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
 python -m pip install -r requirements.txt
 pause
--- run.cmd
+++ run.cmd
@@ -1,5 +1,8 @@
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.7.9-amd64\
+@REM set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable\
+@REM set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
+
+set PYTHON_DIR=D:\Softwares\python-3.10.8-amd64-portable\
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
 
 python main.py
